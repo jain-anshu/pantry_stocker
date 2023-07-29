e59790e8060fcc4ea40b84807619a57ccb910988
jain-anshu <anshuj@gmail.com>
2023-07-29 16:24:05 -0700

    Initial files for the project

---
+++ db/migrate/20230729225013_create_table_ingredients.rb
@@ -0,0 +1,13 @@
+class CreateTableIngredients < ActiveRecord::Migration[7.0]
+  def change
+    create_table :table_ingredients do |t|
+      t.string :name, null: false
+      t.integer :quantity
+      t.integer :status
+      t.datetime :last_purchased_on
+      t.integer :last_purchase_quantity
+      t.float :last_purchase_price
+      t.timestamps
+    end
+  end
+end
